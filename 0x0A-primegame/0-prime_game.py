d4deec84fb813998ea5cbabb2244cea05d72ccbb
Mouadnait <moadnait6@gmail.com>
2024-07-05 00:52:44 +0100

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,34 +4,32 @@
 """
 
 
+def listPrime(n):
+    """A function that generates a list of primes."""
+    numbers = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            numbers.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return numbers
+
+
 def isWinner(x, nums):
-    """
-    Determines the winner in the prime game using
-    Eratosthenes prime sieving algorithm
-    """
-    Ben = 0
-    Maria = 0
-
-    for round in range(x):
-        playing_numbers = [num for num in range(2, nums[round] + 1)]
-        index = 0
-        # Sieve prime numbers per round
-        while (index < len(playing_numbers)):
-            current_prime = playing_numbers[index]
-            sieve_index = index + current_prime
-            while(sieve_index < len(playing_numbers)):
-                playing_numbers.pop(sieve_index)
-                sieve_index += current_prime - 1
-            index += 1
-        # Determine winner - if number of primes is even player 1 wins
-        # else player 2 wins. Player 2  also wins if there is only one
-        # number to pick from
-        prime_count = (len(playing_numbers))
-        if prime_count and prime_count % 2:
-            Maria += 1
+    """A function that determines the winner of the prime games."""
+    ben_score = maria_score = 0
+    for num in nums[:x]:
+        turn = 0
+        for pick in listPrime(num):
+            turn = 1 - turn
+        if turn:
+            maria_score += 1
         else:
-            Ben += 1
-
-    if Ben == Maria:
+            ben_score += 1
+    if maria_score > ben_score:
+        return "Maria"
+    elif maria_score < ben_score:
+        return "Ben"
+    else:
         return None
-    return 'Ben' if Ben > Maria else 'Maria'
